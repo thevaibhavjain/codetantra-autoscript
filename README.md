7eb7d9370896f7f5452cf3f716559e7d3f588023
0x0is1 <42772562+0x0is1@users.noreply.github.com>
2022-03-03 10:05:37 +0530

    [+] Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# Codetantra autoscript
+Automation Script to automatically connect you to codetatantra online classes (whenever available) with selenium.
+
+### Installation
+
+```
+$ git clone https://github.com/0x0is1/codetantra-autoscript
+$ cd codetantra-autoscript
+$ python -m pip install -r requirememnts.txt --user
+```
+### Setting configuration
+* Open [`config.json`](./config.json) file and set up as per your data
+
+![preview-1](./previews/preview-1.png)
+
+**Note: Change it as per your data, else it won't work.**
+
+### Run
+
+```
+$ cd codetantra-autoscript
+$ python autoscript.py
+```
+
+### Preview
+
+![preview-2](./previews/preview-2.png)
+
+### Requirememnts
+
+* Python 3.5+
+* Requests-python
+* BeautifulSoup-python
+* Selenium-python
+* Json-python
+
+## Disclaimer
+
+We do not promote any kind of misuse of this tool to get attendance or any other exploitation. Use it your own risk.
+
+We will definitely take it down if Codetantra want us to do.
+
+## License
+
+This tool is licensed under [The MIT Licence 2022](./LICENSE.md) for free and fair use.
---
+++ autoscript.py
@@ -0,0 +1,103 @@
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+import requests, time, json
+from bs4 import BeautifulSoup
+
+with open("config.json", "r") as f:
+    config = json.load(f)
+
+json_token = ""
+active_sessions = []
+BASE_URL = f"https://{config['university_name_codetantra']}.codetantra.com"
+
+service = Service(config["webdriver_executable_path"])
+
+chrome_options = Options()
+chrome_options.add_argument("--use-fake-ui-for-media-stream")
+
+driver = webdriver.Chrome(options=chrome_options, service=service)
+driver.maximize_window()
+
+
+USERNAME = config["username"]
+PASSWORD = config["password"]
+
+def login(username, password):
+    url = BASE_URL + "/r/l/p"
+    data = f"i={username}&p={password}"
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Referer": config["myclass_url"]
+    }
+    response = requests.post(url, headers=headers, data=data)
+    return response.headers["Set-Cookie"]
+
+def fetch_meetings(wtj_token):
+    url = BASE_URL + "/secure/rest/dd/mf"
+    time_curr = int(time.time()*1000)
+    data = '{"minDate":'+ str(time_curr-(15*3600000)) +',"maxDate":' + str(time_curr+(15*3600000)) + ',"filters":{"showSelf":true,"status":"started,ended,scheduled"}}'
+    headers={
+        "cookie": wtj_token,
+        "Referer": BASE_URL + "/secure/tla/mi.jsp",
+    }
+    response = requests.post(url, headers=headers, data=data)
+    for i in response.json()["ref"]:
+        if i["status"] == "started": return (i["_id"], i["title"])
+    return None, None
+
+def fetch_meeting(cookie, meeting_id):
+    url = BASE_URL + "/secure/tla/jnr.jsp?m="+meeting_id
+    headers = {
+        "cookie": cookie,
+        "Referer": BASE_URL + "/secure/tla/mi.jsp",
+    }
+    response = requests.get(url, headers=headers)
+    return response.content.decode('utf-8')
+
+def get_session_url(raw_data):
+    soup = BeautifulSoup(raw_data, "html.parser")
+    frame = soup.find("iframe", {"id": "frame"})
+    return frame.get("src")
+
+def get_session_token(url):
+    response = requests.head(url, allow_redirects=False)
+    return response.headers['location']
+
+def findnclick_xpath(xpath):
+    time.sleep(10)
+    listen = driver.find_element_by_xpath(xpath)
+    listen.click()
+
+def connect2class(url):
+    driver.get(url)
+    findnclick_xpath("/html/body/div[2]/div/div/div[1]/div/div/span/button[1]")
+    findnclick_xpath("/html/body/div[2]/div/div/div[1]/div/span/button[1]")
+    findnclick_xpath("/html/body/div/main/section/div/header/div/div[1]/div[1]/button")
+    findnclick_xpath("/html/body/div[1]/main/section/div[1]/div/div/div[2]/div[1]/div[2]/div/div/div/div/div/div[2]/span")
+
+def main():
+    global json_token
+    while True:
+        print("[*] Fetching meetings..")
+        try:
+            mid, title = fetch_meetings(json_token)
+        except Exception:
+            json_token = login(USERNAME, PASSWORD)
+            continue
+        if (mid, title) != (None, None) and mid not in active_sessions:
+            print("[+] Live meeting found: ", title)
+            meeting_data = fetch_meeting(json_token, mid)
+            print("[*] Getting session url...")
+            sess_url = get_session_url(meeting_data)
+            sess_url = get_session_token(sess_url)
+            print("[+] Connecting to the class...")
+            connect2class(sess_url)
+            active_sessions.append(mid)
+        else: print("[-] No live meeting found.. skipping")
+        time.sleep(config["refresh_time"])
+
+if __name__ == '__main__':
+    json_token = login(USERNAME, PASSWORD)
+    main()
